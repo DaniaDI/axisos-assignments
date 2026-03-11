827f119d730d4a6f2e2fb861fb1e35ccf50dbf2d
Dania Danial <daniaisead2002@gmail.com>
2026-03-11 23:02:04 +0200

    Add assignment js var

---
+++ WebFun/JS/hw/var.js
@@ -0,0 +1,26 @@
+var favoriteAnimal = "cat";// or let same 
+console.log(favoriteAnimal);
+
+var a = 3;
+var b = 5;
+console.log( a + b);
+
+var age = 24;
+console.log("I am" + " " + age + " " +"Years old.");
+
+var country = "Palestine" ;// or let same 
+country = "China";
+console.log(country);
+
+const PI = 3.14;
+console.log(PI);
+
+var favoriteColor = "Pink";// or let same 
+console.log(favoriteColor );
+
+var x = 5 ;
+var y = 10 ;
+console.log( x + y);
+
+var isSanny = true ; // or let same 
+console.log(isSanny);
